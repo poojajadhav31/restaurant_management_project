99722d15089c592cb7bc223b7d9b9b13f76fc14f
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-05 23:35:55 +0530

    20026

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from accounts.models import Customer
+from menu.models imporrt Product 
 
-# Create your models here.
+ORDER_STATUS_CHOICES = [
+    ('pending', 'Pending'),
+    ('preparing','Preparing'),
+    ('ready','Ready'),
+    ('completed','Completed'),
+    ('cancelled','Cancelled'),
+]
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orderss')
+
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='guest_orders')
+
+        items = models.ManyToManyField(Product, related_name='product_orders')
+        total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+        status = models.CharField(
+        max_length=20,
+        choices-ORDER_STATUS_CHOICES,
+        default='pending')
+
+        created_at = models.DateTimeField(auto_now_add=True)
+
+        def __str__(self):
+            return f"Order#{self.id} - rs{self.total_amount}"
