dd389bce5445746aceaddee51d6e6e78c25e3f79
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-05 23:03:10 +0530

    20026

--- account/models.py
+++ account/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=100, blank=True, null=True)
-    phone_number = models.CharField(length=15, blnak=True, null=True)
+    phone_number = models.CharField(max_length=15, blnak=True, null=True)
 
     def __str__(self):
         return self.user.username
