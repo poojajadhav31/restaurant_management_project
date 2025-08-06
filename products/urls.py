707b84b31e224e3811dca0302ab4df3ff80ecc0c
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-06 17:49:36 +0530

    Auto-save products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import menu_list
 
 urlpatterns = [
-    path('items/', ItemView.as_view(), name='item-list'),
+    path('menu/',menu_list, name='menu-list'),
 ]
