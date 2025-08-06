8db42e7249f911d6a3875e6af0fe12fbc1269716
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-06 14:57:00 +0530

    20027

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import UserProfile
 
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin)
+list_display = ['name','email', 'phone','created_at']
+search_fields =['name','email','phone']
+
 
