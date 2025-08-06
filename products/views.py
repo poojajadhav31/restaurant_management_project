e0dbc82e55522716ba21b6e2d5eec26e531a9ed6
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-06 17:48:10 +0530

    Auto-save products/views.py

--- products/views.py
+++ products/views.py
@@ -5,6 +5,22 @@ from rest_framework.response import Response
 def menu_list(request):
     menu = [
         {
-            
+            "name" : "Panner Butter Masala",
+            "description" : "Soft panner cubs in creamy toamto gravy",
+            "price" : "180.00"
+        },
+
+        {
+            "name" : "Veg Biryni",
+            "description" : "Aromatic rice with mixed vegetables and spices",
+            "price" : "150.00"
+        },
+
+        {
+            "name" : "Masala Dosa",
+            "description" : "Crispy dosa stoffed with spicy mashed patatoes",
+            "price" : 70.00
         }
+
     ]
+    return Response(menu)
