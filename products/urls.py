6d222954b635bd04b2499dae8da35e6b395805bc
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-07 13:09:01 +0530

    23870

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import menu_list
+from .views import menu_api
 
 urlpatterns = [
-    path('menu/',menu_list, name='menu-list'),
+    path('menu/',menu_api, name='menu-api'),
 ]
