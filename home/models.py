8b438f201982783b09eacff7d836bca2c573813a
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-14 20:13:56 +0530

    31895

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class feedback(models.Model):
+    comment = models.TextField()
+    submmited_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return f"feedback {self.id} - {self.submmited_at.strtime('%Y-%m%d')}"
