b957ebd465ba4bf9460d5128bd62dd4f8611b01c
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-15 16:11:02 +0530

    31901

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ import requests
 from django.conf import settings
 from django.shortcuts import render
 from .forms import  ContactForm
-from products.models import products
+from products.models import Product
 from django import feedbackform
 from .models import feedback
 
@@ -35,13 +35,11 @@ def custom_404_view(request, exception):
 
 
 def menu_view(request):
-    menu_items = [
-        {"name" : "Paneer Tikka", "description":"Grilled cottage cheese with spices","price":150},
-        {"name" : "Veg Biryani", "description":"Aromatic rice with vegetables and spices", "price":180},
-        {"name" :  "Butter Chicken","description":"Creamy chiken curry","price":220},
-    ]    
-
-    return render(request,"home/menu_list.html", {"menu_items":,menu_items})
+    menu_items = Product.objects.all()
+    return render(request,"home/menu.html", {
+        "menu_items":,menu_items,
+        "restaurant_name":"Tasty Bites"
+    })
 
 def contact_view(request):
     form = ContactForm(request.POST)
