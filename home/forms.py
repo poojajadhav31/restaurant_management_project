80b037a11eaef763b15476222c20b0b02faf8938
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-15 15:30:36 +0530

    31897

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 from .models import feedbck
 
+class ContactForm(forms.Form):
+    name = forms.Charfield(max_length=100, required=True,widget=forms.TextInput(attrs={
+        'placeholder': "Your Name"
+    }))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'placeholder': "Your Email"
+    }))
+    message = forms.Charfield(
+        required=False,
+        widget = forms.Textarea(attrs={'rows':4, 'placeholder': 'Yuor message (optional)'}),
+    )
 
 class feedbckForm(forms.ModelForm):
     class Meta:
