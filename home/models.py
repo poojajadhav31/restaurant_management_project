4c73d5a450901b242aaab2ac88ef948c77f7d0a2
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-15 17:19:38 +0530

    31898

--- home/models.py
+++ home/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    submmited_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name} ({self.email})"
+
 class feedback(models.Model):
     comment = models.TextField()
     submmited_at = models.DateTimeField(auto_now_add=True)
