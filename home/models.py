a7aca0db1fa62cbf2b13b4051eba30fab1e3c7f4
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-18 14:00:57 +0530

    34234

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class RestaurantInfo(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
+    opening_hours = models.TextField(blank=True)
     
     def __str__(self):
         return self.name
