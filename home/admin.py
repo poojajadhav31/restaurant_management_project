d952195474efd993ef639ea4f280feb6d2d46c8e
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-18 14:04:04 +0530

    34234

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import RestaurantInfo
 
 # Register your models here.
+admin.site.register(RestaurantInfo)
+opening_hours = "Mon-Fri: 11 AM-9PM\nSat-Sun: 10AM-10PM"
