3b4d19b0b27e673734e8fcf2e5b8d434a7dad259
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-18 18:53:11 +0530

    34227

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
     
 
     def __str__(self):
