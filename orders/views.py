4a35a541647bfa5903982d39feb211439a8c9bc4
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-19 21:52:00 +0530

    35441

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Order
 
 # Create your views here.
+def get_cart_item_count(request):
+    if request.user.is_authenticated:
+        try:
+            order = Order.get(user=request.user, status='pending')
+            return order.items.count()
+        except Order.DoesNotExist:
+            return 0
+        else:
+            return 0
