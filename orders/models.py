2131c4832303f3aefab0798ec4a13a1b4df9d0d3
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-19 21:46:07 +0530

    35441

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import Customer
-from menu.models imporrt Product 
+from menu.models import Product 
 
 ORDER_STATUS_CHOICES = [
     ('pending', 'Pending'),
@@ -18,23 +18,26 @@ class Order(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='orderss')
+        related_name='orderss'
+    )
 
     customer = models.ForeignKey(
         Customer,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='guest_orders')
+        related_name='guest_orders'
+    )
 
-        items = models.ManyToManyField(Product, related_name='product_orders')
-        total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-        status = models.CharField(
+    items = models.ManyToManyField(Product, related_name='product_orders')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(
         max_length=20,
         choices-ORDER_STATUS_CHOICES,
-        default='pending')
+        default='pending'
+    )
 
-        created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-        def __str__(self):
-            return f"Order#{self.id} - rs{self.total_amount}"
+    def __str__(self):
+        return f"Order #{self.id} - rs{self.total_amount}"
