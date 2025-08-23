d769492802ae45f1f5b039719a07ba76d2106c90
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-23 23:14:04 +0530

    37397

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path("login/", views.login_view, name="login"),
+    path("logout/",views.logout_view, name="logout"),
 ]
