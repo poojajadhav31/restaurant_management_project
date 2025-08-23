e7df450f2df5e854269dc8bb7d1279d1dd35f568
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-23 23:12:21 +0530

    37397

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+        else:
+            form = AuthenticationForm()
+
+        return render(request,"accounts/login.html",{"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
