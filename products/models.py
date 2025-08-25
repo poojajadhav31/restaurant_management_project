5546d6691db9502cb04bf1ef5588566dd0fb4501
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-25 20:38:03 +0530

    38364

--- products/models.py
+++ products/models.py
@@ -10,3 +10,12 @@ class Product(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class Special(models.Model):
+    item_name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.item name} - {self.date}"
