b8fce6a39d6287d1ab09b34386a07d3ea32028d4
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-26 17:25:41 +0530

    38366

--- home/models.py
+++ home/models.py
@@ -6,6 +6,7 @@ class RestaurantInfo(models.Model):
     phone_number = models.CharField(max_length=20)
     opening_hours = models.TextField(blank=True)
     logo = models.ImageField(upload_to='restaurant_logos/', blank=True, null=True)
+    image = models.ImageField(upload_to='restaurant_images/', blank=True, null=True)
     about_image = models.ImageField(upload_to='restaurant_about/', blank=True, null=True)
     
     def __str__(self):
