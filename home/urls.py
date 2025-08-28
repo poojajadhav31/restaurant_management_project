832e795fda990110e85ee9ec996f71d2c6ef6c51
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-08-28 23:04:22 +0530

    39489

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
    path('about/', views.about_view, name = 'about'),
    path('menu/', menu_view, name='menu_list'),
    path('contact/' views.contact_view, name "contact"),
+   path('contact/success/', views.contact_success, name="contact_success")
    path('reservations/' views,reservations_view, name="reservations"),
    path('feedback/',feedback_view, name='feedback'),
    path('faq/' , faq_view, name="faq"),
